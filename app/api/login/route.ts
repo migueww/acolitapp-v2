31527cacf575670c01f2aa5db7f86db0cd8f52e1
Miguel <miguew.png@gmail.com>
2024-12-06 15:57:56 -0300

    add mongodb connection to login page

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,7 +1,22 @@
-export async function GET() {
-    return new Response(JSON.stringify({ message: "Hello, world!" }), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { username, password } = await req.json();
+
+    const client = await clientPromise;
+    const db = client.db("acolitapp-db");
+
+    const user = await db.collection("users").findOne({ username });
+
+    if (!user || user.password !== password) {
+      return NextResponse.json({ error: "Usuário ou senha incorretos" }, { status: 401 });
+    }
+
+    return NextResponse.json({ message: "Login bem-sucedido", user });
+  } catch (error) {
+    console.error("Erro no servidor:", error);
+    return NextResponse.json({ error: "Erro no servidor" }, { status: 500 });
+  }
 }
-  
